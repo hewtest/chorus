8c1146d628326f2639df7ed92712b1ec51701a7c
Michel Milano <mmilano@alpinenow.com>
2015-02-21 16:49:18 -0800

    WIP on making the job dialog use messaging icons

--- app/assets/javascripts/dialogs/job_result_detail_dialog.js
+++ app/assets/javascripts/dialogs/job_result_detail_dialog.js
@@ -11,10 +11,48 @@ chorus.dialogs.JobResultDetail = chorus.dialogs.Base.extend({
             this.model = new chorus.models.JobResult({jobId: this.job.id, id: 'latest'});
             this.model.fetch();
         }
+        
+//         this.model.get("action")
     },
 
     setup: function () {
         this.title = t("job.result_details.title", {jobName: this.job.name()});
+
         this.render();
+    },
+
+    additionalContext: function () {
+        //console.log ("additionalContext");
+        //console.log ("1: "+ this.model.status); 
+        //console.log ("2: "+ this.model.status() );
+        //console.log ("3: "+ this.status ); undefined
+        //console.log ("4: "+ this.status() );
+        //console.log ("5: "+ status );
+        //console.log ("6+: "+ this.model.get('status') );
+        //console.log ("7: "+ this.get("status") );
+        // console.log ("8: "+ this.job.get('status') );
+         //console.log ("10: "+ this.model.jobTaskResults);
+         //.get('status') );
+         //console.log ("12: "+ this.job.jobTaskResults);
+    
+        
+        return {
+            statusDisplay: this.taskStatus(),
+        };
+    },
+    
+    // jobStatus: map the success or failure status to visuals
+    taskStatus: function () {
+        return ("smelly");
+//         if (this.model.status === "success" ) {
+//             console.log ("success");
+//         }
+//         else if (this.model.status === "failure" ) {
+//             console.log ("failure");
+//         }
+    
     }
+// t("job.result_details.status_success") = Successful
+// t("job.result_details.status_failure") = Task failed
+
 });
