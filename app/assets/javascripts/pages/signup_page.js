fc6004d69797209fc3d21b1b8d21fa83bb753ac1
Hew Wolff <hewwolff@sonic.net>
2015-04-11 17:15:40 -0700

    try different approach for signup page

--- app/assets/javascripts/pages/signup_page.js
+++ app/assets/javascripts/pages/signup_page.js
@@ -1,10 +1,4 @@
 chorus.pages.SignupPage = chorus.pages.Bare.extend({
-    setup:function () {
-        this.mainContent = new chorus.views.Signup({model:chorus.session, el:this.el});
-    },
-
-    render:function () {
-        this.mainContent.render();
-        return this;
-    }
+    constructorName: "SignupPage",
+    templateName: "signup"
 });
